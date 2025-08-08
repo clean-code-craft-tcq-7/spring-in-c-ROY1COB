3c76bee2b9ec985ef745115463805ca805173431
Mohana <mohana.priya2@in.bosch.com>
2025-08-08 12:41:34 +0530

    Updated Minimum code

--- stats.c
+++ stats.c
@@ -3,8 +3,21 @@
 
 struct Stats compute_statistics(const float* numbers, int count) {
     float avg =0.0f;
+    float Min = 0.0f;
+    float Max = 0.0f;
+
     for(int i=0;i<=4;i++)
     avg +=numbers[i];
-     
-    avg = avg/count;
-}struct  stats s;
+    for (int i = 0; i < count; i++) {
+        if (numbers[i] < Min) {
+            Min = arr[i];
+        }
+        if (numbers[i] > Max) {
+            Max = arr[i];
+        }
+    }
+struct  stats s;
+s.average = avg/count;
+s.max= Max;
+s.min = Min; 
+}
